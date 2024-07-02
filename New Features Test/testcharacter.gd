93456cba2ebe94f13b47af7dc0caf05e0c841954
Orpheo <169149102+OrpheoOnChain@users.noreply.github.com>
2024-07-02 15:56:02 +0200

    New SpriteSheet For Player
    New Test Sprite Sheet For Player

--- New Features Test/testcharacter.gd
+++ New Features Test/testcharacter.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
+
+const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,14 +16,32 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	#Get input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	#Flip the sprite 
+	if direction > 0: 
+		animated_sprite.flip_h = false
+	elif direction < 0: 
+		animated_sprite.flip_h = true
+	
+	# Play animations 
+	
+	if is_on_floor(): 
+		if direction == 0: 
+			animated_sprite.play("idle")
+		else: 
+			animated_sprite.play("movement")
+	else: 
+		animated_sprite.play("jump")
+		
+	# Movement x assis 
 	if direction:
 		velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
